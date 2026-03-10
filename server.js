59e1098cb9b6b34c4208c3cdd3923d06c4448b5d
VtthuLoc <vothithuloc11.6@gmail.com>
2026-03-10 09:52:51 +0700

    fix mongodb atlas connection

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', express.static(path.join(__dirname, 'admin')));
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(
+process.env.MONGO_URI,
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(() => console.log("MongoDB Atlas connected"))
 .catch(err => console.log(err));
 
