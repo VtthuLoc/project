a5631cb668d5c7f6cbee49b204923955ad7f3622
VtthuLoc <vothithuloc11.6@gmail.com>
2026-03-10 08:55:51 +0700

    update server

--- server.js
+++ server.js
@@ -469,7 +469,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server đang chạy tại http://localhost:${PORT}`);
 });
